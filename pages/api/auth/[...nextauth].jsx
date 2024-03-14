62315cd4086c3a99f9cd75a116df5b932502b5ae
Denver <csdedwards@gmail.com>
2024-03-14 03:15:57 -0400

    fix

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 });
