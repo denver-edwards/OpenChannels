ee2a0ca51fb6f9023e3fc7b247885dc92069e726
Denver <csdedwards@gmail.com>
2024-04-06 13:57:10 -0400

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { useState } from "react";
+import { Squash as Hamburger } from "hamburger-react";
+
+export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <nav className="bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-5 rounded dark:bg-gray-800 shadow-xl">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link href="/" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            OpenChannels
+          </span>
+        </Link>
+
+        <div className="flex items-center">
+          <button
+            type="button"
+            className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 md:hidden block"
+          >
+            <svg
+              className={"w-5 h-5 fill-gray-500"}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+            </svg>
+            <svg
+              className={"w-5 h-5 fill-gray-500"}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+
+          <button
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Hamburger size={20} toggled={showMenu} toggle={setShowMenu} />
+          </button>
+        </div>
+
+        <div
+          className={
+            "w-full md:block md:w-auto" +
+            (!showMenu
+              ? " hidden"
+              : " transition-transform duration-500 ease-linear")
+          }
+          id="mobile-menu"
+        >
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <Link
+              href="/"
+              className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              aria-current="page"
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            >
+              About
+            </Link>
+            <Link
+              href="/contact"
+              className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Contact
+            </Link>
+          </ul>
+
+          <ul className="flex">
+            <Link href="" className="text-white ">
+              Sign in
+            </Link>
+            <Link href="" className="text-white border rounded px-2 py-1.5">
+              Sign up
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
