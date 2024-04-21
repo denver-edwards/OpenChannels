8bf81b6857a522a8c5d4542b8666017e4037dbce
Denver <csdedwards@gmail.com>
2024-04-21 14:19:08 -0400

    added lucide

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import { useState } from "react";
-import { Squash as Hamburger } from "hamburger-react";
+import { Menu, Search } from "lucide-react";
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <nav className="bg-white border border-gray-700 px-2 sm:px-4 py-5 rounded bg-gray-800 shadow-xl">
+    <nav className="border border-gray-700 px-2 sm:px-4 py-5 rounded bg-gray-800 shadow-xl">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link href="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-gray-400">
             OpenChannels
           </span>
         </Link>
@@ -21,7 +21,7 @@ export default function Header() {
             onClick={() => setShowMenu(!showMenu)}
           >
             <span className="sr-only">Open main menu</span>
-            <Hamburger size={20} toggled={showMenu} toggle={setShowMenu} />
+            <Menu size={20} toggled={showMenu} toggle={setShowMenu} />
           </button>
         </div>
 
@@ -47,27 +47,16 @@ export default function Header() {
             >
               Contact
             </Link>
-
+            {/**
             <div className="relative flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 absolute left-3 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 0a8 8 0 100 16A8 8 0 008 0zM6 8a2 2 0 114 0 2 2 0 01-4 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <Search color="White" className="translate-x-8" />
               <input
                 type="text"
                 placeholder="Search..."
-                className="pl-10 px-3 py-2 text-gray-800 border border-gray-700 rounded-md focus:outline-none focus:border-gray-900"
+                className="pl-10 px-3 py-2 text-gray-300 bg-gray-600 border border-gray-700 rounded-md focus:outline-none focus:border-gray-900"
               />
             </div>
-
+**/}
             <Link
               href=""
               className="block py-2 pr-4 pl-3 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white"
@@ -75,7 +64,7 @@ export default function Header() {
               Sign in
             </Link>
             <Link
-              href=""
+              href="/signup"
               className="block py-2 pr-4 pl-3 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-gray-800 md:hover:text-white hover:bg-gray-700 border bg-white rounded transition-all"
             >
               <p className="px-2 py-1.5 hover:text-white font-semibold">
