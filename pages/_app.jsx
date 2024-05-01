73dc7baa7d616d11df683b4aa7b086dc6733ee86
Denver <csdedwards@gmail.com>
2024-05-01 00:14:45 -0400

    favicon added + nextauth 5

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 
@@ -9,6 +10,18 @@ export default function App({
 }) {
   return (
     <>
+      <Head>
+        <link rel="icon" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="icon" href="/favicon/favicon.ico" alt="OpenChannels Icon" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <SessionProvider session={session}>
         <ToastContainer />
         <Component {...pageProps} />
